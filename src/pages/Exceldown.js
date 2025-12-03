918df450db9c830eb459715887c7df2fd40ac295
bitterfleabane <mkndk22@dankook.ac.kr>
2025-12-03 18:49:31 +0900

    엑셀이름변경!

--- src/pages/Exceldown.js
+++ src/pages/Exceldown.js
@@ -7,6 +7,7 @@ export default function ExcelDownload({ eventCode }) {
   const [baseHeaders, setBaseHeaders] = useState([]);
   const [questionColumns, setQuestionColumns] = useState([]);
   const [rows, setRows] = useState([]);
+  const [eventName, setEventName] = useState("");
 
   const headerMap = {
     studentNum: "학번",
@@ -33,6 +34,7 @@ export default function ExcelDownload({ eventCode }) {
         setBaseHeaders(res.data.baseHeaders || []);
         setQuestionColumns(res.data.questions || []);
         setRows(res.data.rows || []);
+        setEventName(res.data.eventName || "");
       } catch (e) {
         console.error("엑셀 데이터 조회 오류:", e);
       }
@@ -83,7 +85,7 @@ export default function ExcelDownload({ eventCode }) {
     const ws = XLSX.utils.json_to_sheet(excelData);
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, "신청자목록");
-    XLSX.writeFile(wb, `${res.data.eventName}_신청자목록.xlsx`);
+    XLSX.writeFile(wb, `${eventName}_신청자목록.xlsx`);
   };
 
 
